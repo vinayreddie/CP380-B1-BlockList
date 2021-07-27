190050307019a263645b0fdd418a56c5ffa5d7df
bildzeitung <bildzeitung@gmail.com>
2021-07-27 00:00:40 -0400

    Update tests

--- CP380-B1-TestBlock/TestCalculateHash.cs
+++ CP380-B1-TestBlock/TestCalculateHash.cs
@@ -15,7 +15,7 @@ namespace TestBlock
             // NOTE: inputString is == "0001-01-01 12:00:00 AM--0-[]"
             var dt = new System.DateTime(0);
             var block = new Block(dt, "", new List<Payload>());
-            Assert.AreEqual("wPdk2JeNwKVj6W81T9QZsJrUg6RR+E5kS+Gjo33lm5I=", block.Hash);
+            Assert.AreEqual("wPdk2JeNwKVj6W81T9QZsJrUg6RR-E5kS-Gjo33lm5I", block.Hash);
         }
 
         [TestMethod]
@@ -33,27 +33,27 @@ namespace TestBlock
             var dt = new System.DateTime(0);
             var block = new Block(dt, "", new List<Payload>());
             block.Mine(2);
-            Assert.AreEqual("CCtB4okuetq1eFoWjmlGSqm9MQw5bQVDgK8mjasBCws=", block.Hash);
+            Assert.AreEqual("CCtB4okuetq1eFoWjmlGSqm9MQw5bQVDgK8mjasBCws", block.Hash);
         }
 
         [TestMethod]
         public void TestBlockWithPreviousHash()
         {
             var dt = new System.DateTime(0);
-            var previousHash = "wPdk2JeNwKVj6W81T9QZsJrUg6RR+E5kS+Gjo33lm5I=";
+            var previousHash = "wPdk2JeNwKVj6W81T9QZsJrUg6RR-E5kS-Gjo33lm5I";
             var block = new Block(dt, previousHash, new List<Payload>());
-            Assert.AreEqual("S5gde9qo8tOzzioa5XvboAgAg+UbkUqf3hkR3yGSIgU=", block.Hash);
+            Assert.AreEqual("VSOB14OM14XY32DWV3r2nQUin2HdTfjD6_9Yv6nV4wo", block.Hash);
         }
 
         [TestMethod]
         public void TestMineBlockWithPreviousHash()
         {
             var dt = new System.DateTime(0);
-            var previousHash = "wPdk2JeNwKVj6W81T9QZsJrUg6RR+E5kS+Gjo33lm5I=";
+            var previousHash = "wPdk2JeNwKVj6W81T9QZsJrUg6RR-E5kS-Gjo33lm5I";
             var block = new Block(dt, previousHash, new List<Payload>());
             block.Mine(2);
-            Assert.AreEqual(1980, block.Nonce);
-            Assert.AreEqual("CCbdM7TlXH8fJgbQKk8BaoYj2nOVBgx0sMs5P0lHhJ0=", block.Hash);
+            Assert.AreEqual(4586, block.Nonce);
+            Assert.AreEqual("CCsSo2Mav34YLFkeGObUEqxr3VSTkOmRqAQbYo1AYhU", block.Hash);
         }
 
         [TestMethod]
@@ -65,7 +65,7 @@ namespace TestBlock
             var dt = new System.DateTime(0);
             var payload = new List<Payload>() { new Payload("test", TransactionTypes.BUY, 0, "item") };
             var block = new Block(dt, "", payload);
-            Assert.AreEqual("HDrIu8h6RPfm9vyb6LYwU3XJN7INQ6ab44s/egmLfi8=", block.Hash);
+            Assert.AreEqual("HDrIu8h6RPfm9vyb6LYwU3XJN7INQ6ab44s_egmLfi8", block.Hash);
         }
 
         [TestMethod]
@@ -76,11 +76,11 @@ namespace TestBlock
                 new Payload("test", TransactionTypes.BUY, 0, "item"),
                 new Payload("test", TransactionTypes.SELL, 3, "second-item") 
             };
-            var previousHash = "wPdk2JeNwKVj6W81T9QZsJrUg6RR+E5kS+Gjo33lm5I=";
+            var previousHash = "wPdk2JeNwKVj6W81T9QZsJrUg6RR-E5kS-Gjo33lm5I";
             var block = new Block(dt, previousHash, payload);
             block.Mine(2);
-            Assert.AreEqual(1635, block.Nonce);
-            Assert.AreEqual("CCiy4uRQmu1hcU4NFyPXK8E47bIYrDH5PxMiBJuOUB8=", block.Hash);
+            Assert.AreEqual(3294, block.Nonce);
+            Assert.AreEqual("CCqiT6gm4F45Okpi-Pz1zaMD9iWKcfGX2RGyRbma4cI", block.Hash);
         }
     }
 }
