7c038454b3ddb1a05598672f500e082a45f9697a
vinayreddie <62783830+vinayreddie@users.noreply.github.com>
2021-08-09 00:45:03 -0400

    Update Payload.cs

--- CP380-B1-BlockList/Models/Payload.cs
+++ CP380-B1-BlockList/Models/Payload.cs
@@ -1,4 +1,3 @@
-﻿
 namespace CP380_B1_BlockList.Models
 {
     public enum TransactionTypes
@@ -8,6 +7,17 @@ namespace CP380_B1_BlockList.Models
 
     public class Payload
     {
+        public string User { get; set; }
+        public TransactionTypes TransactionType { get; set; }
+        public int Amount { get; set; }
+        public string Item { get; set; }
         // TODO
+        public Payload(string user, TransactionTypes TransactionTypes, int amount, string item)
+        {
+            User = user;
+            TransactionType = TransactionTypes;
+            Amount = amount;
+            Item = item;
+        }
     }
 }
